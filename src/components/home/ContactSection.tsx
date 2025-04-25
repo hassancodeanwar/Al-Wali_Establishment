665b6bf97c60a729ba6d612a078110e18474e3bf
Hassan Anwar <126827134+hassancodeanwar@users.noreply.github.com>
2025-04-25 14:49:46 +0300

    Update ContactSection.tsx

--- src/components/home/ContactSection.tsx
+++ src/components/home/ContactSection.tsx
@@ -84,14 +84,17 @@ const ContactSection: React.FC = () => {
                 </div>
               </div>
             </div>
-            
             {/* Map Preview */}
             <div className="mt-8 rounded-lg overflow-hidden h-[200px] bg-gray-200 dark:bg-gray-700">
-              <img 
-                src="https://images.pexels.com/photos/270348/pexels-photo-270348.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
-                alt="Map Location"
-                className="w-full h-full object-cover"
-              />
+              <iframe 
+                src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d277.3446896307443!2d30.319112512578446!3d30.337396705804377!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1sen!2seg!4v1745581618639!5m2!1sen!2seg" 
+                width="600" 
+                height="450" 
+                style={{ border: 0 }} 
+                allowFullScreen 
+                loading="lazy" 
+                referrerPolicy="no-referrer-when-downgrade"
+              ></iframe>
             </div>
           </div>
         </div>
